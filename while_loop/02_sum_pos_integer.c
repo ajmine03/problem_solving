2aa5168bb84854351863005522a37e1f6bf41b92
ajmine03 <ajmineadil@gmail.com>
2025-06-04 21:50:14 +0600

    added

--- while_loop/02_sum_pos_integer.c
+++ while_loop/02_sum_pos_integer.c
@@ -1,10 +1,28 @@
 /*Sum of Positive Integers Until 0
 
-Write a C program that prompts the user to input a series of integers until the user stops entering 0 using a while loop. 
+Write a C program that prompts the user to input a series of integers
+ until the user stops entering 0 using a while loop.
 Calculate and print the sum of all the positive integers entered*/
 
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int Number, Sum = 0;
+
+    while (1)
+
+    {
+        printf("Input:\n");
+        scanf("%d", &Number);
+        if (Number == 0)
+        {
+            break;
+        }
+        if (Number > 0)
+        {
+            Sum += Number;
+        }
+    }
+    printf("Sum : %d", Sum);
+    return 0;
 }
