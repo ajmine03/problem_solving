4eea2128aa2af399ac67fdd0d77544f984805897
ajmine03 <ajmineadil@gmail.com>
2025-07-10 13:42:16 +0600

    add

--- C/n_base2n_base.c
+++ C/n_base2n_base.c
@@ -1,25 +1,52 @@
-// N base to 10 base conversion.
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
+// Function to convert base-N to base-10
+int baseN_to_base10(int digits[], int length, int baseN) {
+    int result = 0;
+    for (int i = 0; i < length; i++) {
+        result += digits[i] * pow(baseN, length - 1 - i);
+    }
+    return result;
+}
 
-int main(){
-int i, j , k, l ;
-int base ;
-int r1,r2,r3,r4 ;
-scanf("%d %d %d %d", &i,&j,&k,&l);
-printf("input base");
-scanf("%d",&base);
+// Function to convert base-10 to base-M
+void base10_to_baseM(int decimal, int baseM) {
+    int result[64]; // supports large base values
+    int index = 0;
 
-r1 = 1 * i * pow(base,base);
-r2 = 1 * j * pow(base,base-1);
-r3 = 1 * k * pow(base,base-2);
-r4 = 1 * l * pow(base,base-3);
-printf("%d\n",r1+r2+r3+r4);
+    while (decimal > 0) {
+        result[index++] = decimal % baseM;
+        decimal /= baseM;
+    }
 
+    printf("Converted to base-%d: ", baseM);
+    for (int i = index - 1; i >= 0; i--) {
+        printf("%d", result[i]);
+    }
+    printf("\n");
+}
 
-return 0;
+int main() {
+    int baseN, baseM, digits[10], length;
 
+    printf("Enter source base (N): ");
+    scanf("%d", &baseN);
 
+    printf("Enter target base (M): ");
+    scanf("%d", &baseM);
 
+    printf("Enter number of digits: ");
+    scanf("%d", &length);
+
+    printf("Enter digits (most significant to least): ");
+    for (int i = 0; i < length; i++) {
+        scanf("%d", &digits[i]);
+    }
+
+    int decimal = baseN_to_base10(digits, length, baseN);
+    printf("Decimal value: %d\n", decimal);
+    base10_to_baseM(decimal, baseM);
+
+    return 0;
 }
