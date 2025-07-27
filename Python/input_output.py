21b388447e746dae1b6c0f265ed0cf6692fe8e7d
ajmine03 <ajmineadil@gmail.com>
2025-07-27 11:16:28 +0600

    changes

--- Python/input_output.py
+++ Python/input_output.py
@@ -1,5 +1,9 @@
 print("input todo")
-user_input = input("enter something: ")
-print("You entered:", user_input)
-print("output todo")
-print("End of script")
+user_input1 = input("tasks1:")
+user_input2 = input("tasks2:")
+user_input3 = input("tasks3:")
+user_input4 = input("tasks4:")
+
+todos=[user_input4 ,user_input1,user_input2,user_input3]
+
+print(todos)
