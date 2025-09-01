5d24b215f004c035967eea75482373eae5cc756a
ajmine03 <ajmineadil@gmail.com>
2025-09-01 20:48:19 +0600

    added

--- C/1048.c
+++ C/1048.c
@@ -12,13 +12,13 @@ int main()
     else if (A > 400.00 && A <= 800.00)
     {
         printf("Novo salario: %.2f\n",(A+(12*A/100)));
-        printf("Reajuste ganho: %.2f\n",((1*A2/100)));
+        printf("Reajuste ganho: %.2f\n",((12*A/100)));
         printf("Em percentual: 12 %%\n");
     }
     else if (A > 800.00 && A <= 1200.00)
     {
         printf("Novo salario: %.2f\n",(A+(10*A/100)));
-        printf("Reajuste ganho: %.2f\n",((1*A0/100)));
+        printf("Reajuste ganho: %.2f\n",((10*A/100)));
         printf("Em percentual: 10 %%\n");
     }
     else if (A > 1200.00 && A <= 2000.00)
