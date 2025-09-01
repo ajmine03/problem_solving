705a5cfe6b395c5b2ab33d3a642da700b3ccff38
ajmine03 <ajmineadil@gmail.com>
2025-09-01 22:41:31 +0600

    added

---
+++ C/1957.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+    int A ;
+    scanf("%d",&A);
+    if (A<10)
+    {
+        printf("%d\n",A);
+    }
+    
+       
+else if (A==10)
+{
+    printf("A\n");
+}
+
+else if (A==11)
+{
+    printf("B\n");
+}
+
+else if (A==12)
+{
+    printf("C\n");
+}
+
+else if (A==13)
+{
+    printf("D\n");
+}                                          ///next time
+
+else if (A==14)
+{
+    printf("E\n");
+}
+else if (A==15)
+{
+    printf("F\n");
+}
+
+
+return 0;
+    }
+
+    
