7ef971bbd40de99ef5963b6f09fda90acecfa062
ajmine03 <ajmineadil@gmail.com>
2025-09-01 15:53:54 +0600

    added

---
+++ C/1805.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    long long sum;
+    long long A , B ;
+    scanf("%lld %lld",&A,&B);
+    
+    
+    printf ("%lld\n",sum=B*(B+1)/2 - (A-1)*A/2);
+    return 0;
+
+}
