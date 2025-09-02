68d5fac7fd9aa4bcc4f5e468b0e5d54857ad6cf5
ajmine03 <ajmineadil@gmail.com>
2025-09-02 15:01:41 +0600

    added

--- C/1042.c
+++ C/1042.c
@@ -3,25 +3,57 @@ int main()
 {
     int A , B , C ;
     scanf("%d %d %d",&A,&B,&C);
-    if(A>B && B>C && A>C)
+    if(A>B && C>B)
     {
-        printf("%d\n",A);
-        printf("%d\n",B);
-        printf("%d\n",C);
+        if (A>C)
+        {
+            printf("%d\n",B);
+            printf("%d\n",C);
+            printf("%d\n",A);
+        }
+        else if (C>A)
+        {
+            printf("%d\n",B);
+            printf("%d\n",A);
+            printf("%d\n",C);
+        }
+        
+        
     }
-    else if (B>C && C>A && B>A)
+    else if (C>A && B>A)
     {
-        printf("%d\n",B);
-        printf("%d\n",C);
-        printf("%d\n",A);
+        if (C>B)
+        {
+           printf("%d\n",A);
+           printf("%d\n",B);
+           printf("%d\n",C);
+        }
+        else if (B>C)
+        {
+           printf("%d\n",A);
+           printf("%d\n",C);
+           printf("%d\n",B);
+        }
+        
     }
-    else if (C>A && C>B && A>B)
+    else if (A>C && B>C)
     {
-        printf("%d\n",C);
-        printf("%d\n",A);
-        printf("%d\n",B);
+        if (B>A)
+        {
+           printf("%d\n",C);
+           printf("%d\n",A);
+           printf("%d\n",B);
+        }
+        else if (A>B)
+        {
+           printf("%d\n",C);
+           printf("%d\n",B);
+           printf("%d\n",A);
+        }
+        
+        
     }
-        printf(" \n");
+        printf("\n");
         printf("%d\n",A);
         printf("%d\n",B);
         printf("%d\n",C);
